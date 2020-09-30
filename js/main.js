ef368753d4bfc564edc5d2687e1ba7b2f89fc4d0
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-09-30 16:11:20 -0700

    change animation time

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// flashPageAnimation ****************************************
+const effectTime = 3; //Seconds
+
 const welcomeMessageH1 = document.querySelector("#welcome_message h1");
 
 const welcomeMessageP = document.querySelector("#welcome_message p");
@@ -8,16 +11,16 @@ const welcomePageGo = document.querySelector("#go_home");
 
 setTimeout(function(){
     welcomeMessageH1.classList.remove("visibility_hidden")
-}, 2200);
+}, effectTime / 2 * 1000 + 200);
 
 setTimeout(function(){
     welcomeMessageP.classList.add("border_right_none")
-}, 2000);
+}, effectTime / 2 * 1000);
 
 setTimeout(function () {
     welcomePageImage.classList.remove("opacity_zero")
-}, 4200);
+}, effectTime * 1000 + 200);
 
 setTimeout(function () {
     welcomePageGo.classList.remove("visibility_hidden")
-}, 4400);
+}, effectTime * 1000 + 400);
