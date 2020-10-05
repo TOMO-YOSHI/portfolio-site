b6aca3c3bf6037ff35542243120b8746e8849659
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-10-05 04:13:25 -0700

    create contact page

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,13 @@
 // PageTransitionAnimation ****************************************
 import { pageTransitionAnimationWithArguments, pageSlideTransitionAnimationWithArguments } from './pageTransition/pageTransitionAnimation.js';
 
-pageTransitionAnimationWithArguments("#go_to_welcom", "#profile_body", "./index.html");
+// pageTransitionAnimationWithArguments("#go_to_portfolio", "#contact_body", "./portfolio.html");
 
-pageSlideTransitionAnimationWithArguments("#go_to_skills", "#profile_main", "./skills.html", "slide_out_left");
+pageSlideTransitionAnimationWithArguments("#go_to_portfolio", "#contact_main", "./portfolio.html", "slide_out_right");
 // pageSlideTransitionAnimationWithArguments("#go_to_skill", "#profile_body", "./profile.html");
 
 
 // glovalNav ****************************************
 import { globalNav } from './globalNav/globalNav.js';
 
-globalNav("profile");
+globalNav("contact");
