5f6260b8278f881875549371fd04afd7c7878896
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-10-06 20:37:09 -0700

    make each_work page

---
+++ js/each_work.js
@@ -0,0 +1,13 @@
+// PageTransitionAnimation ****************************************
+import { pageTransitionAnimationWithArguments, pageSlideTransitionAnimationWithArguments } from './pageTransition/pageTransitionAnimation.js';
+
+pageTransitionAnimationWithArguments("#go_to_welcom", "#profile_body", "./index.html");
+
+pageSlideTransitionAnimationWithArguments("#go_to_skills", "#profile_main", "./skills.html", "slide_out_left");
+// pageSlideTransitionAnimationWithArguments("#go_to_skill", "#profile_body", "./profile.html");
+
+
+// glovalNav ****************************************
+import { globalNav } from './globalNav/globalNav.js';
+
+globalNav("profile");
