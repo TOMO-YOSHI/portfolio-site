f17304775954aec6e667793f959b1c2046f92677
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-10-11 18:47:54 -0700

    implement contentful

--- js/each_work.js
+++ js/each_work.js
@@ -3,9 +3,9 @@ import '../style.css';
 // PageTransitionAnimation ****************************************
 import { pageTransitionAnimationWithArguments, pageSlideTransitionAnimationWithArguments } from './pageTransition/pageTransitionAnimation.js';
 
-pageTransitionAnimationWithArguments("#go_to_welcom", "#profile_body", "./index.html");
+// pageTransitionAnimationWithArguments("#go_to_welcom", "#profile_body", "./index.html");
 
-pageSlideTransitionAnimationWithArguments("#go_to_skills", "#profile_main", "./skills.html", "slide_out_left");
+pageSlideTransitionAnimationWithArguments("#go_to_works", "#each_work_main", "./works.html", "slide_out_right");
 // pageSlideTransitionAnimationWithArguments("#go_to_skill", "#profile_body", "./profile.html");
 
 
@@ -13,3 +13,27 @@ pageSlideTransitionAnimationWithArguments("#go_to_skills", "#profile_main", "./s
 import { globalNav } from './globalNav/globalNav.js';
 
 globalNav("profile");
+
+// Get Content **************************************
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: "cggsnbtxpwpk",
+  accessToken: "OWFLFySicDm2GDH9jnz3VLOdN8_2ojMCgcM64S0Mirs",
+});
+
+// client.getEntry("47uMEVsOZHp4SxnBvOnkJh").then(function (entry) {
+//   // logs the entry metadata
+//   console.log(entry.sys);
+
+//   // logs the field with ID title
+//   console.log(entry.fields.workName);
+// });
+
+client
+  .getEntries({
+    content_type: "portfolio",
+    "fields.workName[in]": "BBS",
+  })
+  .then((response) => console.log(response.items))
+  .catch(console.error);
